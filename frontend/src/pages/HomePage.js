9a05e1098ff4d4380ead145d537710dfbee564d5
Shivahir2003 <shivahir0114@gmail.com>
2024-03-08 18:59:35 +0530

    project completed Chatapp

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,62 @@
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Login from "../components/Authentication/Login";
+import SignUp from "../components/Authentication/SignUp";
+
+const HomePage = () => {
+  const history = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history("/chats");
+    // if (user) history.push("/chats");
+  }, []);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          VaatCheet
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomePage;
